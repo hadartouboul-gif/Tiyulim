11e367879071d854f63188aa135793afd81b6625
hadartouboul-gif <hadartouboul@gmail.com>
2026-07-21 13:21:49 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* טיולים — service worker: network-first (עדכונים זורמים), נפילה למטמון (עבודה אופליין) */
-const CACHE='voyage-v45';
+const CACHE='voyage-v451';
 self.addEventListener('install',e=>{
   self.skipWaiting();
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png']).catch(()=>{})));
